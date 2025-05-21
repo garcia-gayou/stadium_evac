5964108e6ce1c27914aa487966aebdfc21bbf1df
Gayou <65786475+garcia-gayou@users.noreply.github.com>
2025-05-21 12:14:07 +0200

    Create agents.py

---
+++ agents.py
@@ -0,0 +1,12 @@
+class Agent:
+    def __init__(self, x, y, goal):
+        self.position = [x, y]
+        self.goal = goal
+
+    def move_toward_goal(self, speed=0.2):
+        dx = self.goal[0] - self.position[0]
+        dy = self.goal[1] - self.position[1]
+        norm = (dx**2 + dy**2) ** 0.5
+        if norm > 0:
+            self.position[0] += speed * dx / norm
+            self.position[1] += speed * dy / norm
